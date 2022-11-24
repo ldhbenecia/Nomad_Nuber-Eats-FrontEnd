e0d3b620755c1bd09c72928daa24e9ef394b02ea
DongHyeok Lim <zx07050@naver.com>
2022-11-24 18:46:52 +0900

    TailWind Setting End
    TailWind Setting End

--- nuber-eats-frontend/tailwind.config.js
+++ nuber-eats-frontend/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+
+  variants: {
+    extends: {},
   },
   plugins: [],
 }
